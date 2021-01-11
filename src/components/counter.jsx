1d13b717f494be98d1bc9ef5999626415569e984
Don Simon <48922262+ShahriyarHussain@users.noreply.github.com>
2021-01-12 00:42:14 +0600

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 1
+        count: 0,        
     };
+
+    styles = {
+        fontWeight: "bold"
+    };
+
     render() { 
         return (
-            <React.Fragment>
-                <h1>{this.formatCount()}</h1>
-                <button>Increment</button>
+            <React.Fragment>                
+                <span style={this.styles, {fontSize: 15}} className="badge badge-primary m-3">{this.formatCount()}</span>                
+                <button style={this.styles} className="btn btn-secondary btn-sm">Increment</button>
             </React.Fragment>
         );
     }
